b6a8f733eaf442c2e62139e8151b8ba9b5759109
Ramaiah**4444 <chakaliramaiah@gmail.com>
2022-04-26 13:12:51 +0530

    apitesting

---
+++ src/test/java/Sample.java
@@ -0,0 +1,15 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Sample {
+
+	public static void main(String[] args) throws IOException {
+		 FileReader reader = new FileReader("C:\\Users\\chaka\\eclipse-workspace\\APITesting\\src\\test\\resources\\Json.json");
+     JSONParser jsonParser = new JSONParser();
+     
+		
+		
+	}
+
+}
